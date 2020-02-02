75d8f8c054133b6006c5e9d5b8b16ed282ac06b6
李思凡 <li.sifan@qianka.com>
2020-02-02 22:51:58 +0800

    分享

--- src/components/opinion.vue
+++ src/components/opinion.vue
@@ -15,6 +15,7 @@
 <script>
 import Tabbar from'./tabbar'
 import http from '../http'
+import wx from 'weixin-js-sdk'
 export default {
   data() {
     return {
@@ -25,6 +26,60 @@ export default {
   },
   components: {
     Tabbar
+  },
+  mounted() {
+    let href = window.location.href.split('#')[0]
+    let params = {
+      webUrl: href
+    }
+    http.fetchPost('/ncov2019/wxForward', params).then((res) => {
+      const { WXparams, imgUrl, link, title, desc } = res.data
+      wx.config({
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: WXparams.appId, // 必填，公众号的唯一标识
+        timestamp: WXparams.timestamp, // 必填，生成签名的时间戳
+        nonceStr: WXparams.noncestr, // 必填，生成签名的随机串
+        signature: WXparams.signature,// 必填，签名
+        jsApiList: [
+          "updateAppMessageShareData", // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+          "updateTimelineShareData", // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+          "onMenuShareWeibo" // 获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口] 必填，需要使用的JS接口列表
+        ]
+      })
+
+      wx.ready(function(){
+        wx.checkJsApi({
+          jsApiList: [
+            "updateAppMessageShareData", // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+            "updateTimelineShareData", // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+            "onMenuShareWeibo" // 获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口] 必填，需要使用的JS接口列表
+          ]
+        })
+        wx.updateAppMessageShareData({
+          title: title, // 分享标题
+          desc: desc, // 分享描述
+          link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: imgUrl, // 分享图标
+          success: () => {
+          },
+          cancel: () => {
+            alert('您已取消分享！')
+          }
+        })
+        // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+        wx.updateTimelineShareData({
+          title: title, // 分享标题
+          desc: desc, // 分享描述
+          link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: imgUrl, // 分享图标
+          success: () => {
+          },
+          cancel: () => {
+            alert('您已取消分享！')
+          }
+        })
+      })
+    })
   },
   methods: {
     submit() {
